b810f3e026077691a1be7f1a8ad630a3e0d85a73
stanley portillo <stantroom@hotmail.com>
2023-10-28 14:42:58 -0600

    swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="NOMBRE DE LA API",
+ *      description="Api de ejemplo para la clase de desarrollo de apis",
+ *      @OA\Contact(
+ *          email="ia.stanleyportillo@ufg.edu.sv",
+ *          name="Stanley Portillo",
+ *          url="https://localhost/api/"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/respuestaController.php
+++ app/Http/Controllers/respuestaController.php
@@ -4,15 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+
 class respuestaController extends Controller
 {
-    //
-    public function index(){
+    /**
+ * 
+ * Descripcion de prueba para la clase de desarrollo de backend y API desarrollada por todo el equipo.
+ * @return \Illuminate\Http\Response
+ *
+ * @OA\Get(
+ *     path="/api/",
+ *     tags={"ejecutar"},
+ *     summary="Este es un endpoint/metodo para obtener datos y mostrar al usuario",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Se devuelven todos los registros"
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Se devuelve todos los reistros cuando la consulta sea satisfactoria."
+ *     )
+ * ) 
+ */
+    public function index() {
         return response()->json([
-            "succes"=>true,
-            "data"=>"HOLA, SOY WASAUSKY",
-            "message"=>"registro encontrado",
-            "cantidad"=>1
+            "success"=>true, 
+            "data"=>"Hola, soy Stanley Portillo",
+            "message"=>"Registro Encontrado",
+            "ammount"=>1
         ]);
     }
 }
